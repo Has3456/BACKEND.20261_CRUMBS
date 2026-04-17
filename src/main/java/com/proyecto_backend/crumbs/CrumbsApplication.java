4e7ea7eda2165a2902eb2769a68a7794bf1d3ec8
Has <hascass1503@gmail.com>
2026-04-17 08:57:16 -0500

    Se crea proyecto de backend con las dependencias de SpringBoot

---
+++ src/main/java/com/proyecto_backend/crumbs/CrumbsApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_backend.crumbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrumbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrumbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto_backend/crumbs/CrumbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_backend.crumbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrumbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
