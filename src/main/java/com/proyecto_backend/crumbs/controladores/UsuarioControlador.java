b7b57dfd394f326a74cdad5c7b1908d1d1879390
Laura-316 <laura09torres05@gmail.com>
2026-05-19 19:13:22 -0500

    fix: se arreglan los errores de relacion entre los modelos

--- src/main/java/com/proyecto_backend/crumbs/controladores/UsuarioControlador.java
+++ src/main/java/com/proyecto_backend/crumbs/controladores/UsuarioControlador.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController     
-
-
+@RequestMapping("/api/crumbs/usuarios")
 public class UsuarioControlador {
     
    @Autowired
